afba72e63894e8c469e8dd9ecddf26bee58fcfe4
Wenjie Liao <wenjie.liao@jetbrains.com>
2021-04-19 11:10:19 +0200

    branch1

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -17,6 +17,7 @@ public class HelloServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + " branch 1 "+ "</h1>");
         out.println("</body></html>");
     }
 
